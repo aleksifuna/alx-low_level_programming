5b57bf82cb2b26b330f979f72ddf63e7117dd113
Alex Sifuna <aleksifuna@gmail.com>
2023-06-15 15:06:41 -0700

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints message followed by new line to standard error.
+ *
+ * Return: Always 1 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19, \n");
+	return (1);
+}
