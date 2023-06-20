fa60d8ed2400a560c02b3c445e0e43a65951c829
Alex Sifuna <aleksifuna@gmail.com>
2023-06-20 05:38:22 -0700

    added a function that adds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 /**
  * add - returns the sum of two integers.
+ * @i: first integer
+ * @j: second integer
  *
  * Return: sum of two integers
  */
 int add(int i, int j)
 {
-	return (int i+int j);
+	return (i + j);
 }
