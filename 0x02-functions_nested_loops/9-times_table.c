672027c429209ee736dbc22cfbe441a42e773b5d
Alex Sifuna <aleksifuna@gmail.com>
2023-06-20 16:50:06 -0700

    added a function that prints the multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,42 +8,32 @@ void times_table(void)
 {
 	int i, j, k;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j < 10)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if (k < 10)
+
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			else
+			else if (k < 10 && j != 0)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			if (j < 9)
-			{
-				if (k < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			if (j == 9)
-			{
-				_putchar('\n');
-			}
-			j++;
 		}
-		i++;
+		_putchar('\n');
 	}
 }
+
