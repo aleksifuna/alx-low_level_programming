248c335954f44bfe2f9af3cb25d1b601baa6da17
Alex Sifuna <aleksifuna@gmail.com>
2023-06-20 15:56:03 -0700

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,11 @@ int print_last_digit(int n)
 	int l;
 
 	if (n < 0)
-	{
-		n = n * -1;
-	}
+		n =- n;
 	l = n % 10;
+
+	if (l < 0)
+		l= -l;
 	_putchar(l + '0');
 	return (l);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,9 +26,17 @@ void times_table(void)
 			}
 			if (j < 9)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			if (j == 9)
 			{
