98c65c19afd953bfa5349cb1900fbc2efdd1f24f
Alex Sifuna <aleksifuna@gmail.com>
2023-06-26 15:06:49 -0700

    prints n elements of an array of integers followed by new line character

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		if (i < (n - 1))
-			printf(", ");
-		else
-			printf("\n");
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a[i]);
+			if (i < (n - 1))
+				printf(", ");
+			else
+				printf("\n");
+		}
 	}
 }
