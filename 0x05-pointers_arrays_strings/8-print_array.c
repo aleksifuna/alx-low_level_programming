6dfcb95b104df10053f4ee3e06b45cf1987bf984
Alex Sifuna <aleksifuna@gmail.com>
2023-06-26 15:52:15 -0700

    addeda program that prints n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		printf("%d", a[i]);
+		if (i != (n - 1))
 		{
-			printf("%d", a[i]);
-			if (i < (n - 1))
-				printf(", ");
-			else
-				printf("\n");
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
