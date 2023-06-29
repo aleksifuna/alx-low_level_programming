31965f12d0d8cf24958f46b7f5fdf3e21687003d
Alex Sifuna <aleksifuna@gmail.com>
2023-06-29 12:17:24 -0700

    added a function that that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+/**
+ * rot13 - encodes a string using rot13
+ * str: string 
+ *
+ * Return: encoded string.
+ */
+char *rot13(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] = str[i] + 13;
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+			str[i] = str[i] - 13;
+	}
+	return (str);
+}
