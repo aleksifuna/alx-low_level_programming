fa02cab8cc61711be3514f472d6a8f33a9583b51
Alex Sifuna <aleksifuna@gmail.com>
2023-06-29 09:05:39 -0700

    added a function that capitalizes all the words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,8 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
@@ -20,6 +21,7 @@ char *cap_string(char *str)
 				str[i] == '{' || str[i] == '}' ||
 				i == 0)
 			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
