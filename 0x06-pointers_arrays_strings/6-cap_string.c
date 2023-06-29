d7536b6f5648bd63892026a135f39abe7bac0b49
Alex Sifuna <aleksifuna@gmail.com>
2023-06-29 06:02:28 -0700

    added a function that capitalizes first letter of each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ *
+ * Return: Capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i]
+				=='.' || str[i] == '!' || str[i] == '?'||
+				str[i] == '"' || str[i] == '(' || str[i] ==
+				 ')' || str[i] == '{' || str[i] == '}')
+		{
+			if ( str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
