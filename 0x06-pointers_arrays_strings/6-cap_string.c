b4bcfb7e73c2111b1a64bca44b0a54d98d1e48f8
Alex Sifuna <aleksifuna@gmail.com>
2023-06-29 08:57:59 -0700

    added a function that capitalizes all the words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,14 +9,18 @@ char *cap_string(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-				str[i] == '!' || str[i] == '?' || str[i] == '"'
-				|| str[i] == '(' || str[i] == ')'
-				|| str[i] == '{' || str[i] == '}')
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] = str[i + 1] - 32;
-		}
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||str[i] == '\n'
+				|| str[i] == ',' || str[i] == ';'||
+				str[i] == '.' || str[i] == '!' ||
+				str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' ||
+				str[i] == '{' || str[i] == '}' ||
+				i == 0)
+			str[i] -= 32;
+	}
 	return (str);
 }
+				
