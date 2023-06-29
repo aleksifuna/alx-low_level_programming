3854d0d90d2e5306607afeebbdad9a4abf3b721e
Alex Sifuna <aleksifuna@gmail.com>
2023-06-29 08:17:26 -0700

    added a function that capitalizes all the words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,15 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-                                str[i] == ',' || str[i] == ';' || str[i]
-                                == '.' || str[i] == '!' || str[i] == '?' ||
-                                str[i] == '"' || str[i] == '(' || str[i] ==
-                                 ')' || str[i] == '{' || str[i] == '}')
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"'
+				|| str[i] == '(' || str[i] == ')'
+				|| str[i] == '{' || str[i] == '}')
 		{
 			if (str[i] == '\t')
 				str[i] = ' ';
 			if (str[i + 1] != '\0' && str[i + 1] >= 'a' && str[i + 1] >= 'z')
-				str[i + 1] = str[i + 1] -32;
+				str[i + 1] = str[i + 1] - 32;
 		}
 	return (str);
 }
