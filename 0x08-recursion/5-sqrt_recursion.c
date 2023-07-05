8c52279e2c01bf770ab9bb41545ec7532f150536
Alex Sifuna <aleksifuna@gmail.com>
2023-07-05 17:22:47 +0000

    added a function that find square roots

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+/**
+ * sqt - returns the natural square root.
+ * @n: number to find square root.
+ * @i: index odd number
+ *
+ * Return: Square root
+ */
+int sqt(int n, int i)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (3000);
+	}
+	else
+	{
+		n = n - i;
+		i = i + 2;
+		return (1 + sqt(n, i));
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natual square root of a number
+ * @n: number
+ *
+ * Return: Square root or -1 if no square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (sqt(n, 1) > 1000)
+		return (-1);
+	else
+		return (sqt(n, 1));
+}
