c715074156eed6815bc0dc46fed5f12c3346eaed
Alex Sifuna <aleksifuna@gmail.com>
2023-07-13 07:15:41 +0000

    added a function that allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * _calloc - function that allocates memory for an array.
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: pointer to newly allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *str;
+	unsigned int i;
+
+	if (nmemb == 0)
+	return (NULL);
+	str = malloc(nmemb * size);
+	for (i = 0; i < (nmemb * size); i++)
+		str[i] = '0';
+	return (str);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -4,4 +4,5 @@ How to use the exit function
 What are the functions calloc and realloc from the standard library and how to use them
 
 0-malloc_checked.c - allocates memory using malloc.
-
+1-string_nconcat.c - concatenates two strings
+2-calloc.c: - allocates memory for an array, using malloc
