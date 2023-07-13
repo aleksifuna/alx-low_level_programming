d728c5deabe7ba17091fc7b4e1deb461d2a6f407
Alex Sifuna <aleksifuna@gmail.com>
2023-07-13 17:39:34 +0000

    added a function that reallocates memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 /**
  * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to prev allocated memory
@@ -11,6 +10,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *arr;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -31,7 +31,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	arr = malloc(new_size);
 	if (arr == NULL)
 		return (NULL);
-	strcpy(arr, ptr);
+	for (i = 0; i < old_size; i++)
+		arr[i] = ((char *)ptr)[i];
 	free(ptr);
 	return (arr);
 }
