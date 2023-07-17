6882cd182ba69d27d8d3fd6ee7a7450989c99f5b
Alex Sifuna <aleksifuna@gmail.com>
2023-07-17 23:53:08 +0000

    added a header file that defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGSTRUCT
+#define DOGSTRUCT
+/**
+ * struct dog - dog datatype
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOGSTRUCT */
