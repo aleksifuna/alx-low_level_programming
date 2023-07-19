9133013b4342be48f1a4ce8d44518bce45007f69
Alex Sifuna <aleksifuna@gmail.com>
2023-07-19 13:18:51 +0000

    added a function that searches for a number in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - searches for an integer
+ * @array: array of integers to search
+ * @size: number of elements in the array
+ * @cmp: function that compares compares with our search
+ *
+ * Return: index of first element matching out criteria or otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -6,3 +6,4 @@ Where does a function pointer point to in the virtual memory
 
 0-print_name.c - prints a name
 1-array_iterator.c - itterates through an array using callback
+2-int_index.c - searches for an integer in an array.
