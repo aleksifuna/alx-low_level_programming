e88923793c7790f67b09502b7fc649de70ea7369
Alex Sifuna <aleksifuna@gmail.com>
2023-08-01 23:19:43 +0000

    added afunction that reverses a list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a lsit
+ * @head: pointer to head node 
+ *
+ * Return: pointer to head node of reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current, *following;
+
+	if (*head == NULL)
+		return (NULL);
+	current = *head;
+	if (current->next == NULL)
+		return (current);
+	following = current->next;
+	current->next = NULL;
+	*head = following->next;
+	following->next = current;
+	current =following;
+	while (current->next != NULL)
+	{
+		following = *head;
+		*head = following->next;
+		following->next = current;
+		if (*head == NULL)
+		{
+			*head = following;
+			return (*head);
+		}
+		current = following;
+	}
+	return (NULL);
+}
