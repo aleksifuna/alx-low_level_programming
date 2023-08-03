5b747b57fd10b6568b255936b3f8ebd5440cd402
Alex Sifuna <aleksifuna@gmail.com>
2023-08-03 19:09:33 +0000

    added a function that checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 1 if big else 0
+ */
+int get_endianness(void)
+{
+	unsigned int num;
+	char *ptr;
+
+	num = 1;
+	ptr = (char*)&num;
+	if (*ptr == 1)
+		return (1);
+	else
+		return (0);
+}
