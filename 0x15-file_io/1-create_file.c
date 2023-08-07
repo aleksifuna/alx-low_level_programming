6582ea2bfe2fd7924c8b615adc06b86965a4bea0
Alex Sifuna <aleksifuna@gmail.com>
2023-08-07 18:11:16 +0000

    added a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		len++;
 	}
-	if (text_content == NULL)
-		text_content = "";
 	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
