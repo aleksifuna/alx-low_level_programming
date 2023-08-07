09510a3e4cfc67a92b3d73b7a152400e44551391
Alex Sifuna <aleksifuna@gmail.com>
2023-08-07 18:01:49 +0000

    added a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: null terminated string
+ *
+ * Return: 1 on success -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t len;
+
+	if (filename == NULL)
+		return (-1);
+	len = 0;
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	if (write(fd, text_content, len) != -1)
+		return (1);
+	else
+		return (-1);
+}
