03f9e37f41cacd3de3c72d923b73e7e0d4a45da2
Alex Sifuna <aleksifuna@gmail.com>
2023-09-22 12:53:59 +0000

    added a script that does LDPRELOAD injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/lib_puts.so https://rb.gy/92kor
+wget -P /tmp https://github.com/aleksifuna/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_puts.so
 export LD_PRELOAD=/tmp/lib_puts.so
