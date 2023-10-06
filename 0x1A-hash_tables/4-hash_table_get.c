5167615a340a9d2a5d246d338b621e539e60dbd0
Alex Sifuna <aleksifuna@gmail.com>
2023-10-06 08:23:23 +0000

    added a function that retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	hash_node_t *tmp;
 
+	if (ht == NULL || *key == '\n')
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 	if (index >= ht->size)
 		return (NULL);
