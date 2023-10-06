2b683d95271a3f9a48366bc4ac64cfc0d6eddd03
Alex Sifuna <aleksifuna@gmail.com>
2023-10-06 07:59:44 +0000

    added a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add or update the key/value to
+ * @key: non empty string key
+ * @value: value associated with key
+ *
+ * Return: 1 on success else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+	hash_node_t *new_node;
+
+	if (strlen(key) == 0)
+		return (0);
+	if (ht == NULL)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (0);
+	node = ht->array[index];
+	if (node == NULL)
+	{
+		node = malloc(sizeof(hash_node_t));
+		node->key = malloc(strlen(key) + 1);
+		strcpy(node->key, key);
+		node->value = malloc(strlen(value) + 1);
+		strcpy(node->value, value);
+		node->next = NULL;
+	}
+	else if (strcmp(node->key, key) == 0)
+	{
+		free(node->value);
+		node->value = malloc(strlen(value));
+		strcpy(node->value, value);
+	}
+	else
+	{
+		new_node = malloc(sizeof(hash_node_t));
+		new_node->key = malloc(strlen(key) + 1);
+		strcpy(new_node->key, key);
+		new_node->value = malloc(strlen(value) + 1);
+		strcpy(new_node->value, value);
+		new_node->next = NULL;
+		new_node->next = node;
+		node = new_node;
+	}
+	return (1);
+}
