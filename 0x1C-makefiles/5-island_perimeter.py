35c84e7e9e3b4d30801dc1291f17984586c96867
Alex Sifuna <aleksifuna@gmail.com>
2023-11-24 18:02:23 +0000

    added a function tthat calculates and returns the perimeter of a island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,7 +10,7 @@ def island_perimeter(grid):
     """
     perimeter = 0
     for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i] - 1)):
+        for j in range(1, len(grid[i]) - 1):
             if grid[i][j] == 1:
                 perimeter += 4
                 if grid[i][j-1] == 1:
