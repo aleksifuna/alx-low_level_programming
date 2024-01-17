e8a7f7a12a7fe1ad7b3e8de37aebe6240d35ba83
= <aleksifuna@gmail.com>
2024-01-17 17:09:29 +0000

    added a function that does jump search on a list of elements

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * jump_search - uses jump search algo to search for an element in a list
+ * @array: pointer to first element of the array to search in
+ * @size: the number of elements in the array
+ * @value: value to search for
+ *
+ * Return: index where element is found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jumps, block, prev, i;
+
+	jumps = sqrt(size);
+	block = 0;
+	while (array[block] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", block, array[block]);
+		prev = block;
+		block += jumps;
+		if (block > size - 1)
+			return (-1);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", prev, block);
+	for (i = prev; i <= block; i++)
+	{
+		if (array[i] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			return (i);
+		}
+	}
+	return (-1);
+}
